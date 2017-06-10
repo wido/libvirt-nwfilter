5cb04c0a3ea8a20819e7c5e87e2a549392abfe52
Wido den Hollander <wido@widodh.nl>
2017-06-10 20:45:42 +0200

    Use build-in libvirt filters against MAC and IPv4 spoofing

--- nwfilter.sh
+++ nwfilter.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 virsh define nwfilter-test_dom.xml
-virsh nwfilter-define nwfilter-test_mac.xml
 virsh nwfilter-define nwfilter-test_ipv4.xml
 virsh nwfilter-define nwfilter-test_ipv6.xml
 virsh nwfilter-define nwfilter-test.xml
