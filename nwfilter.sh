d171dba09e603eb3e21c2c67f7552f207cfabe4e
Wido den Hollander <wido@widodh.nl>
2017-06-14 17:36:30 +0200

    Put all filters in one file and try to get it working

--- nwfilter.sh
+++ nwfilter.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-virsh define nwfilter-test_dom.xml
-virsh nwfilter-define nwfilter-test_ipv4.xml
-virsh nwfilter-define nwfilter-test_ipv6.xml
 virsh nwfilter-define nwfilter-test.xml
+virsh define nwfilter-test_dom.xml
 
